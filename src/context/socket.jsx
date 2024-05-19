15a1c193a9a4bc4be8a86036a34a2bab5e37b7ef
Prajil K <prajilk20017@gmail.com>
2024-05-19 22:01:16 +0530

    added withCredentials in socket

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_CHAT_SERVER_URL);
+export const socket = io(import.meta.env.VITE_CHAT_SERVER_URL, {
+  withCredentials: true,
+});
 
 const SocketContext = createContext();
 export const SocketProvider = ({ children }) => {
